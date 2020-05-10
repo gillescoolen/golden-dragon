1cd4f201fa6fbddf4365b52efd33241350b7f38c
Gilles Coolen <gilleswhc@gmail.com>
2020-05-10 20:02:42 +0200

    Feature/database migrations (#15)
    * Add dish description to database documentation
    
    * Add models based on database documentation
    
    * Add migrations based on database documentation
    
    * Update database documentation with dish prefix and suffix
    
    * Add database seeders except DishSeeder
    
    * Add missing category insert
    
    * Fix nullable fields on dishes migration
    
    * Seed dishes table from csv file

--- backend/database/migrations/2020_05_10_154826_create_categories_table.php
+++ backend/database/migrations/2020_05_10_154826_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('password');
+        Schema::create('categories', function (Blueprint $table) {
+            $table->string('name')->primary();
         });
     }
 
@@ -26,6 +25,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 }

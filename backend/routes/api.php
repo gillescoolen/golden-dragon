21463cdba37fc9b969eff7b9ab5e1102ddda15fe
Gilles Coolen <gilleswhc@gmail.com>
2020-05-11 20:13:51 +0200

    Add login, logout and get user routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'AuthController@login')->name('login');
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('user', 'AuthController@user');
+    Route::post('logout', 'AuthController@logout');
 });

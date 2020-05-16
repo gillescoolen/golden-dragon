c8177898d3f007377d52406e4d1a67b4f82e1d4f
Gilles Coolen <gilleswhc@gmail.com>
2020-05-17 00:25:19 +0200

    Add Input and Button component

---
+++ frontend/src/components/UI/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Button extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+button {
+  border: none;
+  padding: 1rem;
+  color: white;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: 700;
+  border-radius: 1rem;
+  background: linear-gradient(124deg, var(--primary), var(--secondary));
+}
+</style>

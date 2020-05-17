9eeae9bd1fbe2d0df34ad2ed324b7462ee1a5d9d
Gilles Coolen <gilleswhc@gmail.com>
2020-05-17 19:39:06 +0200

    Add persistent store and notification library

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,13 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 import auth from './auth';
 import order from './order';
 
 Vue.use(Vuex);
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+});
+
 export default new Vuex.Store({
   modules: {
     auth,
     order
-  }
+  },
+  plugins: [vuexLocal.plugin]
 });

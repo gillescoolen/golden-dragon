1202161960a1c69fadfd9c87a0c801055a70a4e5
Gilles Coolen <gilleswhc@gmail.com>
2020-05-17 19:39:40 +0200

    Fix modal styling on smaller tablets

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -15,17 +15,25 @@ export default class Modal extends Vue {}
 
 <style lang="scss" scoped>
 .modal {
+  padding-top: 3rem;
   width: 100%;
   height: 100%;
   z-index: 100;
   position: absolute;
+  min-width: 30rem;
 
   .content {
     margin: 3rem;
-    padding: 3rem;
     height: 70vh;
     z-index: 200;
+    padding: 3rem;
+    border-radius: 1rem;
     background-color: white;
+    box-shadow: 0 0 1rem 0 #00000036;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
 }
 </style>

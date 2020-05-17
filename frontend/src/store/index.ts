c11aa3b70c791aefee5adb34e016ea1b5961c6c2
Gilles Coolen <gilleswhc@gmail.com>
2020-05-17 13:15:36 +0200

    Fix store module naming

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import Auth from './auth';
-import Order from './order';
+import auth from './auth';
+import order from './order';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    Auth,
-    Order
+    auth,
+    order
   }
 });

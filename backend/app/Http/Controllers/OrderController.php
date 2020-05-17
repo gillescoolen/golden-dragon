ef1c58c4852ae6325f26e98104fda31012fd8592
Gilles Coolen <gilleswhc@gmail.com>
2020-05-17 21:24:51 +0200

    Add order api

---
+++ backend/app/Http/Controllers/OrderController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function all()
+    {
+        return Order::all();
+    }
+
+    public function single($id)
+    {
+        return Order::where('id', '=', $id)->with('allergies')->first();
+    }
+
+    public function create(Request $request)
+    {
+        $order = Order::create([
+            'comment' => $request->get('comment') ? $request->get('comment') : '',
+            'date' => date('Y-m-d')
+        ]);
+
+        foreach ($request->get('order') as $key => $value) {
+            $order->dishes()->attach($value['id'], ['amount' => $value['amount']]);
+            $order->save();
+        }
+
+        return Order::where('id', '=', $order->id)->with('dishes')->first();
+    }
+}

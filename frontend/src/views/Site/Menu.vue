ef301c2c55a17a2a6e43aaa5b13ce8af3070aa44
Gilles Coolen <gilleswhc@gmail.com>
2020-05-24 12:14:43 +0200

    Add site menu page

--- frontend/src/views/Site/Menu.vue
+++ frontend/src/views/Site/Menu.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <h1>This is the Menu page.</h1>
+    <img width="100%" src="@/assets/images/restaurant-menukaart-1-2.jpg" />
+    <img width="100%" src="@/assets/images/restaurant-menukaart-1.jpg" />
   </div>
 </template>

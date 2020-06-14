4eb51454beced2d2979d16736cbe19c2fff04036
Gilles Coolen <gilleswhc@gmail.com>
2020-06-14 15:40:54 +0200

    Small order logic fixes

--- frontend/src/pages/Register/Register.vue
+++ frontend/src/pages/Register/Register.vue
@@ -1,81 +1,82 @@
 <template>
-  <div class="register">
-    <Modal v-if="show">
-      <h1>{{ selectedDish.name }}</h1>
-      <div class="comment">
-        Comment
-        <input v-model="comment" type="text" />
-      </div>
-      <div class="extra">
-        <select
-          v-model="extra"
-          :disabled="!enableSideDish"
-          @change="addSideDish()"
-        >
-          <option :key="99" :value="null" selected>
-            Witte Rijst (Standaard)
-          </option>
-          <option v-for="extra in extras" :key="extra.id" :value="extra">
-            {{ extra.name }}
-          </option>
-        </select>
-      </div>
-      <Button small="true" @click.native="closeModal(selectedDish)">
-        Toevoegen!
-      </Button>
-    </Modal>
-    <div class="container categories">
-      <transition name="fade" mode="out-in">
-        <div v-if="categories.length">
-          <div
-            class="category"
-            v-for="category in categories"
-            :key="category.name"
-          >
-            <h1>{{ category.name }}</h1>
-            <div class="dishes">
-              <Row
-                class="row"
-                v-for="dish in category.dishes"
-                :key="dish.id"
-                :name="getDishName(dish)"
-                :description="dish.description"
-              >
-                <span class="price">€ {{ dish.price.toFixed(2) }}</span>
-                <Button small="true" @click.native="openModal(dish)"
-                  >Toevoegen!</Button
+  <div class="wrapper">
+    <transition name="fade" mode="out-in">
+      <Modal v-if="show" @close="show = false">
+        <h1>{{ selectedDish.name }}</h1>
+        <div class="extra">
+          <Input v-model="comment" label="Gerechtnotitie" type="text" />
+          <select v-model="extra">
+            <option key="null" :value="null" selected>
+              Witte Rijst (Standaard)
+            </option>
+            <option v-for="extra in extras" :key="extra.id" :value="extra">
+              {{ extra.name }}
+            </option>
+          </select>
+        </div>
+        <Button @click.native="closeModal(selectedDish)">
+          Toevoegen!
+        </Button>
+      </Modal>
+    </transition>
+    <div class="register" :class="{ blur: show }">
+      <div class="container categories">
+        <transition name="fade" mode="out-in">
+          <div v-if="categories.length">
+            <div
+              class="category"
+              v-for="category in categories"
+              :key="category.name"
+            >
+              <h1>{{ category.name }}</h1>
+              <div class="dishes">
+                <Row
+                  class="row"
+                  v-for="dish in category.dishes"
+                  :key="dish.id"
+                  :name="getDishName(dish)"
+                  :description="dish.description"
                 >
-              </Row>
+                  <span class="price">€ {{ dish.price.toFixed(2) }}</span>
+                  <Button small="true" @click.native="openModal(dish)"
+                    >Toevoegen!</Button
+                  >
+                </Row>
+              </div>
             </div>
           </div>
-        </div>
-      </transition>
-    </div>
-    <div class="container order">
-      <h1>Bestelling</h1>
-      <transition-group name="list-complete" tag="div" class="dishes">
-        <Row
-          class="row list-complete-item"
-          v-for="dish in order"
-          :key="dish.id"
-          :name="getDishName(dish)"
-        >
-          <span class="price"
-            >€ {{ (dish.price * dish.amount).toFixed(2) }}</span
+        </transition>
+      </div>
+
+      <div class="container order">
+        <h1>Bestelling</h1>
+        <transition-group name="list-complete" tag="div" class="dishes">
+          <Row
+            class="row list-complete-item"
+            v-for="dish in order"
+            :key="`${dish.id}-${dish.comment}`"
+            :name="getDishName(dish)"
+            :description="dish.comment"
           >
-          <Button small="true" @click.native="removeDish(dish)">Minder!</Button>
-          <span class="amount">{{ dish.amount }}</span>
-          <Button small="true" @click.native="addDish(dish)">Meer!</Button>
-        </Row>
-      </transition-group>
-      <div class="footer">
-        <Button :disabled="!order.length" @click.native="clear()"
-          >Verwijderen</Button
-        >
-        <h2>Totaal € {{ price.toFixed(2) }}</h2>
-        <Button :disabled="!order.length" @click.native="submit()"
-          >Afrekenen</Button
-        >
+            <span class="price"
+              >€ {{ (dish.price * dish.amount).toFixed(2) }}</span
+            >
+            <Button small="true" @click.native="removeDish(dish)"
+              >Minder!</Button
+            >
+            <span class="amount">{{ dish.amount }}</span>
+            <Button small="true" @click.native="addDish(dish)">Meer!</Button>
+          </Row>
+        </transition-group>
+        <div class="footer">
+          <Button :disabled="!order.length" @click.native="clear()"
+            >Verwijderen</Button
+          >
+          <h2>Totaal € {{ price.toFixed(2) }}</h2>
+          <Button :disabled="!order.length" @click.native="submit()"
+            >Afrekenen</Button
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -87,6 +88,7 @@ import { Category, Dish } from '@/types';
 import api from '@/utils/api';
 
 import Row from '@/components/UI/Row.vue';
+import Input from '@/components/UI/Input.vue';
 import Modal from '@/components/UI/Modal.vue';
 import Button from '@/components/UI/Button.vue';
 import DishItem from '@/components/UI/DishItem.vue';
@@ -95,8 +97,9 @@ import DishItem from '@/components/UI/DishItem.vue';
   components: {
     DishItem,
     Button,
-    Row,
-    Modal
+    Modal,
+    Input,
+    Row
   }
 })
 export default class Register extends Vue {
@@ -107,11 +110,12 @@ export default class Register extends Vue {
   selectedDish: Dish | null = null;
   comment = '';
   show = false;
-  enableSideDish = false;
 
   addDish(dish: Dish) {
     const index = this.order.findIndex(item => item.id === dish.id);
-    index != -1
+    console.log(this.comment);
+
+    index != -1 && this.order[index].comment == this.comment
       ? this.order[index].amount++
       : this.order.push({
           ...dish,
@@ -128,7 +132,6 @@ export default class Register extends Vue {
 
   openModal(dish: Dish) {
     this.show = true;
-    this.enableSideDish = true;
     this.selectedDish = dish;
   }
 
@@ -144,7 +147,9 @@ export default class Register extends Vue {
   }
 
   removeDish(dish: Dish) {
-    const index = this.order.findIndex(item => item.id === dish.id);
+    const index = this.order.findIndex(
+      item => item.id === dish.id && item.comment === dish.comment
+    );
     const item = this.order[index];
 
     item.amount > 1 ? this.order[index].amount-- : this.order.splice(index, 1);
@@ -187,6 +192,16 @@ export default class Register extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  overflow-y: hidden;
+
+  .extra {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+}
+
 .register {
   width: 100%;
   display: flex;
